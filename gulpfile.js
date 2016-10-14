67782e989b084a0a173a0cb1363c5dc3c50867c1
後藤和良 <goto@gotoukazuyoshi-no-iMac.local>
2016-10-14 15:30:03 +0900

    upload file

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserSync = require("browser-sync");
+
+// タスクの設定
+gulp.task("browserSyncTask", function () {
+    browserSync({
+        server: {
+            baseDir: "src" // ルートとなるディレクトリを指定
+        }
+    });
+
+    // srcフォルダ以下のファイルを監視
+    gulp.watch("src/**", function() {
+        browserSync.reload();   // ファイルに変更があれば同期しているブラウザをリロード
+    });
+});
+
+gulp.task('default', ['browserSyncTask']);
